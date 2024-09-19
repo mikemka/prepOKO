adc21aa4a57e671c780a1bf76d48f16f2b9ff991
микемка <mikemka@vk.com>
2024-09-19 11:27:19 +0500

    start front, add login view

--- prep_oko/settings/urls.py
+++ prep_oko/settings/urls.py
@@ -1,6 +1,25 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('dev/admin/', admin.site.urls),
+    path('', include('users.urls')),
+    # path('admin/', admin.site.urls),
+    # path('activities/', include('users.urls')),
+    # path('moderation/', include('users.urls')),
 ]
+
+
+if settings.DEBUG:
+    from django.conf.urls import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    
+    if settings.MEDIA_ROOT:
+        urlpatterns += static.static(
+            prefix=settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT,
+        )
+    
+    urlpatterns += staticfiles_urlpatterns()
--- prep_oko/users/views.py
+++ prep_oko/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+    template_name = 'users/login.html'
+    form_class = AuthenticationForm
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        return render(request, self.template_name, {'form': form})
+
+
